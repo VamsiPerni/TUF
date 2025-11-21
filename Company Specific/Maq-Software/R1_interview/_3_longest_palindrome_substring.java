ca0cee0b56802374ab5db254017eedf3c1852d4a
VamsiPerni <mohanperni777@gmail.com>
2025-11-21 11:27:26 +0530

    Add initial implementation for finding the longest palindromic substring

---
+++ Company Specific/Maq-Software/R1_interview/_3_longest_palindrome_substring.java
@@ -0,0 +1,31 @@
+// -------------------------------------------------------------------------------------
+
+// Desc :- Longest Palindrome Substring , E.g :- aabaadbbabb , ans :- aabaa (OR)
+// bbabb
+// 5. Longest Palindromic Substring :-
+// https://leetcode.com/problems/longest-palindromic-substring/description/
+
+// Method - I :- By using Two-for loops
+// Time Complexity :-
+// Space Comlpexity :-
+
+// import java.util.*; // TRIED - SELF
+
+// public class _3_longest_palindrome_substring {
+
+// public static HashMap<Character, Integer> countFreq() {
+// HashMap<Character, Integer> freq = new HashMap<>();
+
+// return freq;
+// }
+
+// public static void main(String ars[]) {
+// Scanner sc = new Scanner(System.in);
+
+// String str = sc.nextLine();
+
+// HashMap<Character, Integer> res = new HashMap<>();
+
+// sc.close();
+// }
+// }
